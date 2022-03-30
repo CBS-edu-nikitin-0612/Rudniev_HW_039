1387c4076e78e50ad30c3e07345d15b84d255f12
Vladyslav Rudniev <Rudniev.CSdev@gmail.com>
2022-03-30 19:51:42 +0300

    Добавьте файлы проекта.

---
+++ AditionalTask/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace AditionalTask
+{
+    class Base
+    {
+        public void TemplateMethod()
+        {
+            this.Method1();
+            this.Method2();
+        }
+        protected virtual void Method1()
+        {
+            Console.WriteLine("Method1...");
+        }
+        protected virtual void Method2() { }
+    }
+    class Derived : Base
+    {
+        protected override void Method2()
+        {
+            Console.WriteLine("Method2...");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Base instance = new Derived();
+            instance.TemplateMethod();
+        }
+    }
+}
---
+++ Task2/TemplateMethod.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    public abstract class AbstractTemplateMethod
+    {
+        public virtual void TemplateMethod()
+        {
+            this.Method1();
+            //add next method in derived class
+        }
+        protected abstract void Method1();
+    }
+
+    internal class MyClass : AbstractTemplateMethod
+    {
+        public override void TemplateMethod()
+        {
+            base.TemplateMethod();
+            this.Method2();
+        }
+        protected override void Method1()
+        {
+            Console.WriteLine("Method1...");
+        }
+        protected virtual void Method2()
+        {
+            Console.WriteLine("Method2...");
+        }
+    }
+}
